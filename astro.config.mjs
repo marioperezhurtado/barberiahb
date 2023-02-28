80a6eaa85a65c1469fd6b4c0f77e9f6dd566ef3a
Mario Pérez Hurtado <marioph10@gmail.com>
2023-02-28 14:49:02 +0100

    Fixing lighthouse erros and generating sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,33 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
-import tailwind from '@astrojs/tailwind';
+import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
-import image from '@astrojs/image';
+import image from '@astrojs/image'
 
 // https://astro.build/config
-import react from '@astrojs/react';
+import react from '@astrojs/react'
 
 // https://astro.build/config
-import sitemap from '@astrojs/sitemap';
+import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
-import vercel from "@astrojs/vercel/serverless";
+import vercel from '@astrojs/vercel/serverless'
+
+// https://astro.build/config
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://barberiahb.com',
-  integrations: [tailwind(), image(), react(), sitemap()],
-  output: "server",
+  integrations: [
+    tailwind(),
+    image(),
+    react(),
+    sitemap({
+      customPages: ['https://barberiahb.com']
+    })
+  ],
+  output: 'server',
   adapter: vercel()
-});
+})
