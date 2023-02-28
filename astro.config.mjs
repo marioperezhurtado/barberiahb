fc9ed2b2e0dc5583e0d3e6cd6c55ab8818d0655b
Mario Pérez Hurtado <marioph10@gmail.com>
2023-02-28 14:14:49 +0100

    Setting for deploy on vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,24 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
-import image from "@astrojs/image";
+import image from '@astrojs/image';
 
 // https://astro.build/config
-import react from "@astrojs/react";
+import react from '@astrojs/react';
+
+// https://astro.build/config
+import sitemap from '@astrojs/sitemap';
+
+// https://astro.build/config
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), image(), react()]
+  site: 'https://barberiahb.com',
+  integrations: [tailwind(), image(), react(), sitemap()],
+  output: "server",
+  adapter: vercel()
 });
